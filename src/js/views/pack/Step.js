f9ae7e5b6fe0759ce13370ceed585f01c09c95e7
pmzi <pouya.mozafar@gmail.com>
2018-05-19 22:02:49 +0430

    quill integrated with design

--- src/js/views/pack/Step.js
+++ src/js/views/pack/Step.js
@@ -1,27 +1,112 @@
-class Step{
+class Step {
 
-    constructor(){
+    constructor() {
 
         this.initEvents();
 
+        this.initQuill();
+
     }
 
-    initEvents(){
+    initEvents() {
 
-        let that = this;// some stupid action...
+        let that = this; // some stupid action...
 
-        $('.stepCont').click(function(){
+        // Header click change color and change steps
+
+        $('.stepCont').click(function () {
 
             $('#steps>div').removeClass('active')
 
-            $('#steps>div').each((index,elem)=>{
+            // let's change the step
 
-                if(index <= $(this).index()){
+            $('.stepWrapper.forActive').removeClass('forActive')
+            $('.stepWrapper.backActive').removeClass('backActive')
+
+            $('.stepWrapper').each((index, elem) => {
+                if (index < $(this).index() / 2) {
+                    $(elem).addClass('backActive');
+                } else if (index > $(this).index() / 2) {
+                    $(elem).addClass('forActive');
+                }
+            })
+
+            $('#steps>div').each((index, elem) => {
+
+                if (index <= $(this).index()) {
                     $(elem).addClass('active')
                 }
 
             })
         })
+
+    }
+
+    initQuill() {
+
+        let configs = {
+            modules: {
+                toolbar: [
+                    ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+                    ['blockquote', 'code-block', 'image'],
+
+                    [{
+                        'header': 1
+                    }, {
+                        'header': 2
+                    }], // custom button values
+                    [{
+                        'list': 'ordered'
+                    }, {
+                        'list': 'bullet'
+                    }],
+                    [{
+                        'script': 'sub'
+                    }, {
+                        'script': 'super'
+                    }], // superscript/subscript
+                    [{
+                        'indent': '-1'
+                    }, {
+                        'indent': '+1'
+                    }], // outdent/indent
+                    [{
+                        'direction': 'rtl'
+                    }], // text direction
+
+                    [{
+                        'size': ['small', false, 'large', 'huge']
+                    }], // custom dropdown
+                    [{
+                        'header': [1, 2, 3, 4, 5, 6, false]
+                    }],
+
+                    [{
+                        'color': []
+                    }, {
+                        'background': []
+                    }], // dropdown with defaults from theme
+                    [{
+                        'font': []
+                    }],
+                    [{
+                        'align': []
+                    }],
+
+                    ['clean'] // remove formatting button
+                ]
+            },
+            placeholder: 'متن...',
+            theme: 'snow' // or 'bubble'
+        }
+
+        $('.quillEditor').each(() => {
+            let editor = new Quill('.quillEditor:not(.ql-container)', configs);
+
+            editor.format('direction', 'rtl');
+            editor.format('align', 'right');
+        });
+
     }
 
 }
