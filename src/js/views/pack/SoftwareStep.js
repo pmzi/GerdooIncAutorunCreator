906ecd28bddfec8703b62a44a936ee6b4d295c51
pmzi <pouya.mozafar@gmail.com>
2018-05-21 17:57:42 +0430

    software section dsigned + theme section designed

--- src/js/views/pack/SoftwareStep.js
+++ src/js/views/pack/SoftwareStep.js
@@ -1,47 +1,62 @@
-class SoftwareStep{
+class SoftwareStep {
 
-    constructor(){
-
+    constructor() {
         this.initHeaderEvents();
 
+        this.initSoftwareSectionEvents();
     }
 
     // Header methods start
 
-    initHeaderEvents(){
-
+    initHeaderEvents() {
         // for header buttons(search, filter by cat, filter by dvd)
 
-        let that = this;
+        const that = this;
 
-        $('#softwareMenu>header>div>ul>li').click(function(){
-            
+        $('#softwareMenu>header>div>ul>li').click(function () {
             that.changeHeaderSlide($(this).index());
-
         });
+    }
+
+    search() {
 
     }
 
-    search(){
-
-    }
-
-    changeHeaderSlide(index){
-
+    changeHeaderSlide(index) {
         $('#softHeaderSliderWrapper .slideWrapper.forSlide').removeClass('forSlide');
         $('#softHeaderSliderWrapper .slideWrapper.backSlide').removeClass('backSlide');
 
-        $('#softHeaderSliderWrapper .slideWrapper').each((i,elem)=>{
-            if(i<index){
+        $('#softHeaderSliderWrapper .slideWrapper').each((i, elem) => {
+            if (i < index) {
                 $(elem).addClass('backSlide');
-            }else if(i > index){
-                $(elem).addClass('forSlide');   
+            } else if (i > index) {
+                $(elem).addClass('forSlide');
             }
-        })
-
+        });
     }
 
     // Header methods finish
+
+    // Soft list methods
+
+    initSoftwareSectionEvents() {
+        $('#softwares ul:not(.softWrapper) li').click(function (e) {
+            e.stopPropagation()
+            
+            $(this).children('ul').slideToggle(500);
+
+            $('#softwares .active').removeClass('active');
+
+            $(this).children('div').toggleClass('active');
+
+            if($(this).parent().hasClass('catWrapper')){
+                $('#softwareMenu>footer>ul>li:nth-of-type(3)').removeClass('disabled');
+            }else{
+                $('#softwareMenu>footer>ul>li:nth-of-type(3)').addClass('disabled');
+            }
+
+        });
+    }
 
 }
 
