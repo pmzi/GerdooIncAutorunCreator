6e7a683f02c710d18d5d7bb6fb6050ad4fc67d52
pmzi <pouya.mozafar@gmail.com>
2018-05-24 22:23:49 +0430

    quill handler now handles the video and image adding system

---
+++ src/js/views/pack/QuillHandler.js
@@ -0,0 +1,122 @@
+const 
+
+class QuillHandler {
+
+    constructor() {
+
+        this.initQuills();
+
+    }
+
+    initQuills() {
+        let configs = {
+            modules: {
+                toolbar: [
+                    ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+                    ['blockquote', 'code-block', 'image', 'video'],
+                    [{
+                        'header': 1
+                    }, {
+                        'header': 2
+                    }], // custom button values
+                    [{
+                        'list': 'ordered'
+                    }, {
+                        'list': 'bullet'
+                    }],
+                    [{
+                        'script': 'sub'
+                    }, {
+                        'script': 'super'
+                    }], // superscript/subscript
+                    [{
+                        'indent': '-1'
+                    }, {
+                        'indent': '+1'
+                    }], // outdent/indent
+                    [{
+                        'direction': 'rtl'
+                    }], // text direction
+
+                    [{
+                        'size': ['small', false, 'large', 'huge']
+                    }], // custom dropdown
+                    [{
+                        'header': [1, 2, 3, 4, 5, 6, false]
+                    }],
+
+                    [{
+                        'color': []
+                    }, {
+                        'background': []
+                    }], // dropdown with defaults from theme
+                    [{
+                        'font': []
+                    }],
+                    [{
+                        'align': []
+                    }],
+
+                    ['clean'] // remove formatting button
+                ]
+            },
+            placeholder: 'متن...',
+            theme: 'snow' // or 'bubble'
+        }
+
+        $('.quillEditor').each(() => {
+            let editor = new Quill('.quillEditor:not(.ql-container)', configs);
+            editor.getModule('toolbar').addHandler('video', () => {
+                this.addQuillVideo(editor)
+            });
+            editor.getModule('toolbar').addHandler('image', () => {
+                this.addQuillImage(editor)
+            });
+            editor.format('direction', 'rtl');
+            editor.format('align', 'right');
+        });
+    }
+
+    addQuillVideo(editor) {
+
+        let videoAddress = dialog.showOpenDialog({
+            properties: ['openFile'],
+            filters: [{
+                name: 'video',
+                extensions: ['mkv', 'avi', 'mp4']
+            }]
+        });
+
+        this.insertVideoToEditor(editor, url)(editor, videoAddress)
+
+    }
+
+    addQuillImage(editor) {
+        let imageAddress = dialog.showOpenDialog({
+            properties: ['openFile'],
+            filters: [{
+                name: 'image',
+                extensions: ['png', 'ico', 'jpg', 'jpeg']
+            }]
+        });
+
+        this.insertImageToEditor(editor, imageAddress)
+    }
+
+    insertVideoToEditor(editor, url) {
+        // push image url to rich editor.
+        const range = editor.getSelection();
+
+        editor.insertEmbed(range.index, 'video', 'file:///' + url);
+    }
+
+    insertImageToEditor(editor, url) {
+        // push image url to rich editor.
+        const range = editor.getSelection();
+
+        editor.insertEmbed(range.index, 'image', 'file:///' + url);
+    }
+
+}
+
+new QuillHandler();
--- src/js/views/pack/Step.js
+++ src/js/views/pack/Step.js
@@ -4,8 +4,6 @@ class Step {
 
         this.initEvents();
 
-        this.initQuill();
-
     }
 
     initEvents() {
@@ -40,79 +38,8 @@ class Step {
             })
         })
 
-    }
-
-    initQuill() {
-
-        let configs = {
-            modules: {
-                toolbar: [
-                    ['bold', 'italic', 'underline', 'strike'], // toggled buttons
-                    ['blockquote', 'code-block', 'image','video'],
-                    [{
-                        'header': 1
-                    }, {
-                        'header': 2
-                    }], // custom button values
-                    [{
-                        'list': 'ordered'
-                    }, {
-                        'list': 'bullet'
-                    }],
-                    [{
-                        'script': 'sub'
-                    }, {
-                        'script': 'super'
-                    }], // superscript/subscript
-                    [{
-                        'indent': '-1'
-                    }, {
-                        'indent': '+1'
-                    }], // outdent/indent
-                    [{
-                        'direction': 'rtl'
-                    }], // text direction
-
-                    [{
-                        'size': ['small', false, 'large', 'huge']
-                    }], // custom dropdown
-                    [{
-                        'header': [1, 2, 3, 4, 5, 6, false]
-                    }],
-
-                    [{
-                        'color': []
-                    }, {
-                        'background': []
-                    }], // dropdown with defaults from theme
-                    [{
-                        'font': []
-                    }],
-                    [{
-                        'align': []
-                    }],
-
-                    ['clean'] // remove formatting button
-                ]
-            },
-            placeholder: 'متن...',
-            theme: 'snow' // or 'bubble'
-        }
-
-        $('.quillEditor').each(() => {
-            let editor = new Quill('.quillEditor:not(.ql-container)', configs);
-            editor.getModule('toolbar').addHandler('video', () => {
-                adddd();
-              });
-            editor.format('direction', 'rtl');
-            editor.format('align', 'right');
-        });
-
-    }
+    }    
 
 }
 
 new Step();
-function adddd(){
-    console.log("HERE")
-}
